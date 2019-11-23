dac6691b40e2633419f7420d6b80d7bed4db288c
yamauchi <yamauchi.toshinori1132@gmail.com>
2019-11-24 00:53:28 +0900

    up

---
+++ faster_rcnn/demo.py
@@ -0,0 +1,48 @@
+import argparse
+import matplotlib.pyplot as plt
+
+import chainer
+
+from chainercv.datasets import voc_bbox_label_names
+from chainercv.links import FasterRCNNVGG16
+from chainercv import utils
+from chainercv.visualizations import vis_bbox
+from chainercv.datasets import VOCBboxDataset
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', type=int, default=-1)
+    parser.add_argument('--pretrained-model')
+    parser.add_argument(
+        '--dataset', choices=('voc',), default='voc')
+    # parser.add_argument('image')
+    args = parser.parse_args()
+
+    if args.dataset == 'voc':
+        if args.pretrained_model is None:
+            args.pretrained_model = 'voc07'
+        label_names = voc_bbox_label_names
+
+    model = FasterRCNNVGG16(
+        n_fg_class=len(label_names),
+        pretrained_model=args.pretrained_model)
+
+    dataset = VOCBboxDataset(split='test', year='2007',
+                               use_difficult=True, return_difficult=True)
+    
+    if args.gpu >= 0:
+        chainer.cuda.get_device_from_id(args.gpu).use()
+        model.to_gpu()
+
+    # img = utils.read_image(args.image, color=True)
+    img = dataset[0][0]
+    bboxes, labels, scores = model.predict([img])
+    bbox, label, score = bboxes[0], labels[0], scores[0]
+
+    vis_bbox(
+        img, bbox, label, score, label_names=label_names)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
